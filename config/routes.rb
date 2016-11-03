46830c90a648b38700d6b1e90e11c8639f7db6b4
Igor German <igerman@chicagobooth.edu>
2016-11-03 16:34:08 -0500

    11/3 434

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  
+
+# Routes for Movies
+  get("/", { :controller => "movies", :action => "index" })
+  get("/:id", { :controller => "movies", :action => "show" })
+
+#Routes to create movies
+  get("/movies/new_form", { :controller => "movies", :action => "new_form" })
+  get("/create_movie", { :controller => "movies", :action => "create_row" })
+
 end
