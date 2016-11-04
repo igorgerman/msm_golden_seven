e5e0a339787e7986cb89ee1639bba55af4792024
Igor German <igerman@chicagobooth.edu>
2016-11-04 11:39:41 -0500

    CRUD for Movies
    Finished the CRUD for movies--haven't figured out how to do Director ID

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -19,11 +19,40 @@ class MoviesController <ApplicationController
     @movie.duration = params[:duration]
     @movie.description = params[:description]
     @movie.image_url = params[:image_url]
-    @movie.director_id = params[:director_id]
+    # @movie.director_id = params[:director_id]
 
     @movie.save
 
     render("show")
   end
+
+def edit_form
+    @movie = Movie.find(params[:id])
+end
+
+def update_row
+  @movie = Movie.find_by({ :id => params[:id]})
+
+  @movie.title = params[:title]
+  @movie.year = params[:year]
+  @movie.duration = params[:duration]
+  @movie.description = params[:description]
+  @movie.image_url = params[:image_url]
+  # @movie.director_id = params[:director_id]
+
+  @movie.save
+
+  render("show")
+
+end
+
+def destroy
+  @movie = Movie.find_by({ :id => params[:id]})
+  @movie.destroy
+
+  render
+end
+
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,21 @@
 Rails.application.routes.draw do
 
-# Routes for Movies
-  get("/", { :controller => "movies", :action => "index" })
-  get("/:id", { :controller => "movies", :action => "show" })
+get("/", { :controller => "movies", :action => "index" })
 
 #Routes to create movies
   get("/movies/new_form", { :controller => "movies", :action => "new_form" })
   get("/create_movie", { :controller => "movies", :action => "create_row" })
+
+  # Routes to Read Movies
+    get("/movies", { :controller => "movies", :action => "index" })
+    get("/movies/:id", { :controller => "movies", :action => "show" })
+
+    # UPDATE
+    get('/movies/:id/edit_form', { :controller => 'movies', :action => 'edit_form' })
+    get('/update_movie/:id', { :controller => 'movies', :action => 'update_row' })
+
+    #delete
+    get('/movies/:id/delete', { :controller => 'movies', :action => 'destroy' })
+
 
 end
