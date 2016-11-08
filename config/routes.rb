81b951e6515ae7a806b23b3a045351d7c97f4b21
Igor German <igerman@chicagobooth.edu>
2016-11-07 21:11:39 -0600

    11-7 9.11

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
 get("/", { :controller => "movies", :action => "index" })
 
-#Routes to create movies
+
+  #MOVIES
+  #Routes to create movies
   get("/movies/new_form", { :controller => "movies", :action => "new_form" })
   get("/create_movie", { :controller => "movies", :action => "create_row" })
 
@@ -16,6 +18,43 @@ get("/", { :controller => "movies", :action => "index" })
 
     #delete
     get('/movies/:id/delete', { :controller => 'movies', :action => 'destroy' })
+
+
+    #directors
+    #Create directors
+    get('/directors/new_form', { :controller => 'directors', :action => 'new_form' })
+    get('/create_director', { :controller => 'directors', :action => 'create_row' })
+
+    # READ
+    get('/directors', { :controller => 'directors', :action => 'index' })
+    get('/directors/:id', { :controller => 'directors', :action => 'show' })
+
+    # UPDATE
+    get('/directors/:id/edit_form', { :controller => 'directors', :action => 'edit_form' })
+    get('/update_director/:id', { :controller => 'directors', :action => 'update_row' })
+
+    # DELETE
+    get('/delete_director/:id', { :controller => 'directors', :action => 'destroy' })
+
+
+    #ACTORS
+
+    #CREATE ACTORS
+    get('/actors/new_form', { :controller => 'actors', :action => 'new_form' })
+    get('/create_actor', { :controller => 'actors', :action => 'create_row' })
+
+
+    #READ EM
+    get('/actors', { :controller => 'actors', :action => 'index' })
+    get('/actors/:id', { :controller => 'actors', :action => 'show' })
+
+
+    #UPDATE EM
+    get('/actors/:id/edit_form', { :controller => 'actors', :action => 'edit_form' })
+    get('/update_actor/:id', { :controller => 'actors', :action => 'update_row' })
+
+    #DELETE EM
+    get('/delete_actor/:id', { :controller => 'actors', :action => 'destroy' })
 
 
 end
